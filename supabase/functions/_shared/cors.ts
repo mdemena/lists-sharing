98a2ac72dd87c58228e262fbb665303a4e3d73cb
Marc De Mena <marc.demena@carethy.net>
2025-11-27 22:12:44 +0100

    Adding Shared Users Info

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,6 @@
 // supabase/functions/_shared/cors.ts
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Cambia por orígenes específicos en producción
+  'Access-Control-Allow-Origin': 'http://localhost:5173,https://lists-sharing.mdemena.com', // Cambia por orígenes específicos en producción
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
 }
