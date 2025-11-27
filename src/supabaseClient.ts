bd72315c9d8323bc261e9a9d692f57fabfcde5fe
Marc De Mena <marc.demena@carethy.net>
2025-11-27 21:54:54 +0100

    Adding Shared Users Info

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,12 +4,12 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 // --- Verificación de Variables de Entorno ---
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     // Lanza un error claro si falta alguna clave
-    throw new Error("Missing Supabase configuration variables. Check your VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY in .env file.");
+    throw new Error("Missing Supabase configuration variables. Check your SUPABASE_URL and SUPABASE_ANON_KEY in .env file.");
 }
 
 // --- Creación del Cliente Tipado ---
